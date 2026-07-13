4b5bfca8abfb3d98df0f41c0a86a1d32b7d65d84
Abby Malson <august.malson@gmail.com>
2026-07-13 10:25:19 -0600

    fix: skip hidden directories in search_lazy to prevent hanging on .obsidian/.graphify-cache

--- lib/bot_army_para/para_fs.ex
+++ lib/bot_army_para/para_fs.ex
@@ -415,6 +415,8 @@ defmodule BotArmyPara.ParaFs do
   end
 
   defp recurse_if_needed(name, dir, query, pattern, scope, search_content, para_root, acc) do
+    if String.starts_with?(name, "."), do: return(acc)
+
     search_lazy(Path.join(dir, name), query, pattern, scope, search_content, para_root, acc)
   end
 
@@ -498,3 +500,7 @@ defmodule BotArmyPara.ParaFs do
     end
   end
 end
+
+  defp skip_hidden_dir?(name) do
+    String.starts_with?(name, ".")
+  end
